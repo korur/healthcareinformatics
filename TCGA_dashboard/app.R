b13d1ba5ac5b835e4fa0fda230f7f91aebdd818b
korur <serdar.korur@gmail.com>
2020-01-15 17:27:18 +0100

    tcgaapp_version1

--- TCGA_dashboard/app.R
+++ TCGA_dashboard/app.R
@@ -1,43 +1,86 @@
-# TCGA shinydashboard Web App
-
 library(shiny)
 library(shinydashboard)
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+mean_mutations <- 107
+mean_altered <- 0.23
+patientcount <- 48008
+studycount <- 145
+# Shiny dashboard App
+header <- dashboardHeader(
+    dropdownMenu(type = "notifications", 
+                 notificationItem(
+                     text = "Build by www.dataatomic.com", 
+                     icon = shiny::icon("atom"),
+                     status = "success",
+                     href = "https://www.dataatomic.com"))
+)
+sidebar <- dashboardSidebar(
+    sliderInput("rateThreshold", "Warn when rate exceeds",
+                min = 0, max = 50, value = 3, step = 0.1
+    ),
+    sidebarMenu(
+        menuItem("Data", 
+                 tabName = "data"
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+        menuItem("Dashboard",
+                 tabName = "dashboard"
+        ) 
     )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
 
-# Run the application 
+body <- dashboardBody(
+    
+    fluidRow(
+        # Add a value box for mean mutaion count
+        valueBox(
+            value = mean_mutations,
+            subtitle = "Average mutations", 
+            icon = icon("dna"), color = 'yellow', width = 3
+        ),
+        valueBox(
+            value = mean_altered,
+            subtitle = "Average genome alterations", 
+            icon = icon("percent"), color = 'yellow',width = 3
+        ),
+        valueBox(
+            value = studycount,
+            subtitle = "Studies", 
+            icon = icon("folder-open"),color = 'aqua',width = 3
+        ),
+        valueBox(
+            value = patientcount,
+            subtitle = "Patients", 
+            icon = icon("users"),color = 'aqua',width = 3
+        )
+        ),
+    tabItems(
+        tabItem(tabName = "data", tabBox(
+            title = "Dataatomic",
+            tabPanel("Tab1", "Tab1 content"),
+            tabPanel("Tab2", "Tab2 content")
+        )),
+        tabItem(tabName = "dashboard", 
+                tabBox(
+                    title = "testbox",
+                    tabPanel("Weight", "Tab1 content"),
+                    tabPanel("Height", "Tab2 content")
+                ))
+    ),
+    
+    
+    
+    
+)
+
+
+ui <- dashboardPage(
+    header = header,
+    sidebar = sidebar,
+    body <- body
+)
+
+server <- function(input, output) {}
+
 shinyApp(ui = ui, server = server)
